3adda6a06944744cd856a7ffc71c74792cf6821b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-27 22:38:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
-
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+  
 end
